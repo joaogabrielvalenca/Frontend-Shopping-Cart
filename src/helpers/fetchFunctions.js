b7b5e1ad84e6a33f5375195fbdc16bd32fbcfdc6
joaogabrielvalenca <joaogabrielvalenca@gmail.com>
2023-01-23 12:51:17 -0300

    commitando para testes

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -1,23 +1,27 @@
 export const fetchProduct = async (id) => {
-  if (!id) throw new Error('ID não informado');
+  if (!id) {
+    const erro = new Error('ID não informado');
+    return erro;
+  }
   try {
     const response = await fetch(`https://api.mercadolibre.com/items/${id}`);
     const data = await response.json();
     return data;
   } catch (error) {
     const catchError = 'ID não informado';
-    alert(catchError);
+    return catchError;
   }
 };
 
 export const fetchProductsList = async (search) => {
-  if (search === '') {
+  if (!search) {
     const empty = new Error('Algum erro ocorreu, recarregue a página e tente novamente');
     alert(empty);
   }
   try {
     const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${search}`);
     const data = await response.json();
+    console.log(data)
     return data.results;
   } catch (error) {
     const erro = new Error('Algum erro ocorreu, recarregue a página e tente novamente!');
--- src/tests/fetchProductsList.test.js
+++ src/tests/fetchProductsList.test.js
@@ -5,16 +5,20 @@ import computadorSearch from './mocks/search';
 // implemente seus testes aqui
 describe('Teste a função fetchProductsList', () => {
   it('fetchProductsList é uma função', () => {
-    
+    expect(typeof fetchProductsList).toEqual("function");
   });
-
-  it('fetch é chamado ao executar fetchProductsList', () => {
-
+  it('fetch é chamado ao executar fetchProductsList', async () => {
+    await expect(fetchProductsList('gato')).resolves.toBe(Object)
   });
-
-  it('fetch é chamado com o endpoint correto ao executar fetchProductsList', () => {
-
+  it('fetch é chamado com o endpoint correto ao executar fetchProductsList', async () => {
+    const testFetch = async (search) => {
+      const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${search}`);
+      const data = await response.json();
+      return data;
+    }
+    await expect(testFetch('gato')).toBe(fetchProductsList('gato'))
   });
+  
 
   // it('...', () => {
   // });
